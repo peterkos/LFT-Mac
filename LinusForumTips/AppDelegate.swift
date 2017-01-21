5de898ce8faebcb82f5853b9b0e812b7eaa1b61f
Peter Kos <pkos91@icloud.com>
2017-01-21 16:46:24 -0500

    Added basic project

---
+++ LinusForumTips/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinusForumTips
+//
+//  Created by Peter Kos on 1/21/17.
+//  Copyright © 2017 Peter Kos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ LinusForumTips/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LinusForumTips
+//
+//  Created by Peter Kos on 1/21/17.
+//  Copyright © 2017 Peter Kos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
