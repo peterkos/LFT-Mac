e8d1f2e555eca073c0d7b1f397a6d5c0f4eec742
Zach Ashton <contactme@zachashton.me.uk>
2017-02-21 10:42:49 +0000

    Was going to go further with a script to get videos, but I got called away. Still, this will get the list of videos (maybe do streaming too?)

---
+++ LinusForumTips/getVideos.swift
@@ -0,0 +1,11 @@
+//
+//  getVideos.swift
+//  LinusForumTips
+//
+//  Created by Zach Ashton on 21/02/2017.
+//  Copyright © 2017 Peter Kos. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
